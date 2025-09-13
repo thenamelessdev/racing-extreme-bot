3e71277d0eec0fbf8f0d3d474d31e3b8c5527301
thenamelessdev <aron.minarovits@gmail.com>
2025-09-13 18:53:22 +0200

    added first slash command

--- src/index.js
+++ src/index.js
@@ -3,12 +3,32 @@ const { token } = require('../config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.DirectMessages, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildModeration, GatewayIntentBits.GuildWebhooks, GatewayIntentBits.GuildInvites, GatewayIntentBits.GuildPresences, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildMessageTyping] });
 
-client.once(Events.ClientReady, readyClient => {
+// commands
+const commands = [
+    new SlashCommandBuilder()
+        .setName("Ping")
+        .setDescription("Just a ping command")
+]
+
+client.once(Events.ClientReady, async readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-    client.user.setPresence({
+    await client.user.setPresence({
         activities: [{ name: "Game coming soon", type: 0}],
         status: "online"
     })
+
+    const rest = new REST({ version: "10" }).setToken(token);
+    try {
+        console.log("Registering commands...");
+        await rest.put(
+            Routes.applicationCommands(readyClient.user.id), // Use applicationCommands for global commands
+            { body: commands.map(cmd => cmd.toJSON()) }
+        );
+        console.log("Commands registered.");
+    } catch (error) {
+        console.error("There was an error registering commands:", error);
+    }
+
 });
 
 // prefix commands
@@ -17,6 +37,15 @@ client.once(Events.ClientReady, readyClient => {
 client.on(Events.MessageCreate, (message) => {
     if (message.content == "!ping") {
         message.reply("Pong!");
+    }
+})
+
+// slash commands
+
+// ping command
+client.on(Events.InteractionCreate, (interaction) => {
+    if (interaction.commandName == "ping") {
+        interaction.reply("Pong!")
     }
 })
 
