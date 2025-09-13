1ea7c2d5984452433e5e9cc8a6285134205e7da3
thenamelessdev <aron.minarovits@gmail.com>
2025-09-13 18:44:58 +0200

    added ping prefix command

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { Client, Events, GatewayIntentBits, REST, Routes, SlashCommandBuilder, ActionRowBuilder, EmbedBuilder, Status, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, ButtonBuilder, ButtonStyle, MessageFlags, ContextMenuCommandBuilder, ModalBuilder, ApplicationCommandType } = require('discord.js');
+const { Client, Events, GatewayIntentBits, REST, Routes, SlashCommandBuilder, ActionRowBuilder, EmbedBuilder, Status, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, ButtonBuilder, ButtonStyle, MessageFlags, ContextMenuCommandBuilder, ModalBuilder, ApplicationCommandType, messageLink } = require('discord.js');
 const { token } = require('../config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.DirectMessages, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildModeration, GatewayIntentBits.GuildWebhooks, GatewayIntentBits.GuildInvites, GatewayIntentBits.GuildPresences, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildMessageTyping] });
@@ -10,5 +10,14 @@ client.once(Events.ClientReady, readyClient => {
         status: "online"
     })
 });
+
+// prefix commands
+
+// !ping command
+client.on(Events.MessageCreate, (message) => {
+    if (message.content == "!ping") {
+        message.reply("Pong!");
+    }
+})
 
 client.login(token);
