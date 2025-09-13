9789f0673ada8cd3cac8d71692fb60a5898f5d86
thenamelessdev <aron.minarovits@gmail.com>
2025-09-13 18:41:45 +0200

    added status and some intents and imports

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const { Client, Events, GatewayIntentBits, REST, Routes, SlashCommandBuilder, ActionRowBuilder, EmbedBuilder, Status, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, ButtonBuilder, ButtonStyle, MessageFlags, ContextMenuCommandBuilder, ModalBuilder, ApplicationCommandType } = require('discord.js');
+const { token } = require('../config.json');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.DirectMessages, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildModeration, GatewayIntentBits.GuildWebhooks, GatewayIntentBits.GuildInvites, GatewayIntentBits.GuildPresences, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildMessageTyping] });
+
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+    client.user.setPresence({
+        activities: [{ name: "Game coming soon", type: 0}],
+        status: "online"
+    })
+});
+
+client.login(token);
